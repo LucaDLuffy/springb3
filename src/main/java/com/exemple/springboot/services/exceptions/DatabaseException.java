3f4ff354840e8602542992c39e43d1d512bbc505
LucaDLuffy <LucasRafaeIsaac@gmail.com>
2023-09-11 23:06:50 -0300

    Exception handling - delete

---
+++ src/main/java/com/exemple/springboot/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.exemple.springboot.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
